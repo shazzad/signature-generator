63b10ea388ea84f9870d537fef26b521cf75f2ea
shazzad <sajib1223@gmail.com>
2021-07-16 03:36:43 +0600

    Fix

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,18 @@
                 :companies="companies" 
                 :form="form" 
               >
-                <ActionButtons slot="footer" class="field field-submit" :preview="$refs.preview" :form="form" @generated="generated()" />
+                <ActionButtons 
+                  slot="footer" 
+                  class="field field-submit" 
+                  :form="form"
+                  :canGenerate="canGenerate"
+                  :copyHtmlBtnText="copyHtmlBtnText"
+                  :copySignatureBtnText="copySignatureBtnText"
+                  :copiedHtml="copiedHtml"
+                  :copiedSignature="copiedSignature"
+                  @copySignature="copySignature"
+                  @copyHtml="copyHtml"
+                />
               </SignatureForm>
               <SignatureIntegrations />
             </div>
@@ -31,7 +42,18 @@
                 :form="form"
                 ref="preview"
               />
-              <ActionButtons v-if="previewButtons" class="preview-buttons" :preview="$refs.preview" :form="form" @generated="generated()" />
+              <ActionButtons 
+                v-if="previewButtons" 
+                class="preview-buttons" 
+                :form="form"
+                :canGenerate="canGenerate"
+                :copyHtmlBtnText="copyHtmlBtnText"
+                :copySignatureBtnText="copySignatureBtnText"
+                :copiedHtml="copiedHtml"
+                :copiedSignature="copiedSignature"
+                @copySignature="copySignature"
+                @copyHtml="copyHtml"
+              />
             </div>
           </div>
         </div>
@@ -61,6 +83,8 @@ export default {
       useCache: window.esgConfig.useCache || false,
       previewButtons: window.esgConfig.previewButtons || false,
       companies: window.esgConfig.companies || [],
+      copiedHtml: false,
+      copiedSignature: false,
       form: {
         company: 'AdColony',
         name: '',
@@ -70,12 +94,93 @@ export default {
       }
     }
   },
+  
+  computed: {
+    copySignatureBtnText() {
+      return this.copiedSignature ? 'Copied' : 'Copy Signature'
+    },
+    copyHtmlBtnText() {
+      return this.copiedHtml ? 'Copied' : 'Copy Html'
+    },
+    canGenerate() {
+      if ( ! this.form.name) {
+        return false;
+      }
+
+      if ( this.form.name.split(' ').filter((part) => part).length <= 1 ) {
+        return false;
+      }
+
+      if ( ! this.form.jobTitle.trim() ) {
+        return false;
+      }
+
+      return true;
+    }
+  },
   methods: {
     generated() {
       if (this.useCache) {
         let form = this.form;
         localStorage.setItem('form', JSON.stringify(form));
       }
+    },
+    copySignature: function() {
+      let element = this.$refs.preview;
+
+      if ( ! element ) {
+        alert('Preview element not yet ready');
+        return false;
+      }
+
+      if (document.body.createTextRange) {
+        let range = document.body.createTextRange();
+        range.moveToElementText(element.$el);
+        range.select();
+        document.execCommand('copy');
+
+      } else if (window.getSelection) {
+        let selection = window.getSelection();        
+        let range = document.createRange();
+        range.selectNodeContents(element.$el);
+        selection.removeAllRanges();
+        selection.addRange(range);
+        document.execCommand('copy');
+        selection.removeAllRanges();
+      }
+
+      this.generated();
+      this.copiedSignature = true;
+      setTimeout(() => { this.copiedSignature = false }, 2000)
+    },
+    copyHtml: function() {
+      let element = this.$refs.preview;
+
+      if ( ! element ) {
+        alert('Preview element not yet ready');
+        return false;
+      }
+
+      const el = document.createElement('textarea');
+
+      el.value = element.$el.innerHTML;
+      el.setAttribute('readonly', '');
+      el.style.position = 'absolute';
+      el.style.left = '-9999px';
+      document.body.appendChild(el);
+
+      const selected =  document.getSelection().rangeCount > 0  ? document.getSelection().getRangeAt(0) : false;                                    
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+      if (selected) {
+        document.getSelection().removeAllRanges();
+        document.getSelection().addRange(selected);
+      }
+
+      this.generated();
+      this.copiedHtml = true;
+      setTimeout(() => { this.copiedHtml = false }, 2000)
     }
   },
   mounted() {
--- src/components/ActionButtons.vue
+++ src/components/ActionButtons.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <button type="button" :disabled="canGenerate === false" class="btn primary-btn" @click.prevent="copySignature()">{{ copySignatureBtnText }}</button>
-        <button type="button" :disabled="canGenerate === false" class="btn secondary-btn" @click.prevent="copyHtml()">{{ copyHtmlBtnText }}</button>
+        <button type="button" :title="title" :disabled="canGenerate === false" class="btn primary-btn" @click.prevent="$emit('copySignature')">{{ copySignatureBtnText }}</button>
+        <button type="button" :title="title" :disabled="canGenerate === false" class="btn secondary-btn" @click.prevent="$emit('copyHtml')">{{ copyHtmlBtnText }}</button>
     </div>
 </template>
 
@@ -9,89 +9,16 @@
 export default {
   name: 'ActionButtons',
   props: [
-    'preview',
-    'form'
+    'form',
+    'copiedSignature',
+    'copiedHtml',
+    'copySignatureBtnText',
+    'copyHtmlBtnText',
+    'canGenerate'
   ],
-  data() {
-    return {
-      copiedSignature: false,
-      copiedHtml: false
-    }
-  },
-  methods: {
-    copySignature: function() {
-      let element = this.preview;
-
-      if ( typeof(element) === "undefined" ) {
-        alert('Preview not yet ready');
-        return false;
-      }
-
-      if (document.body.createTextRange) {
-        let range = document.body.createTextRange();
-        range.moveToElementText(element.$el);
-        range.select();
-        document.execCommand('copy');
-
-      } else if (window.getSelection) {
-        let selection = window.getSelection();        
-        let range = document.createRange();
-        range.selectNodeContents(element.$el);
-        selection.removeAllRanges();
-        selection.addRange(range);
-        document.execCommand('copy');
-        selection.removeAllRanges();
-      }
-
-      this.$emit('generated');
-      this.copiedSignature = true;
-      setTimeout(() => { this.copiedSignature = false }, 2000)
-    },
-    copyHtml: function() {
-      const el = document.createElement('textarea');
-
-      el.value = this.preview.innerHTML;
-      console.log(el.value);
-      el.setAttribute('readonly', '');
-      el.style.position = 'absolute';
-      el.style.left = '-9999px';
-      document.body.appendChild(el);
-
-      const selected =  document.getSelection().rangeCount > 0  ? document.getSelection().getRangeAt(0) : false;                                    
-      el.select();
-      document.execCommand('copy');
-      document.body.removeChild(el);
-      if (selected) {
-        document.getSelection().removeAllRanges();
-        document.getSelection().addRange(selected);
-      }
-
-      this.$emit('generated');
-      this.copiedHtml = true;
-      setTimeout(() => { this.copiedHtml = false }, 2000)
-    }
-  },
   computed: {
-    copySignatureBtnText() {
-      return this.copiedSignature ? 'Copied' : 'Copy Signature'
-    },
-    copyHtmlBtnText() {
-      return this.copiedHtml ? 'Copied' : 'Copy Html'
-    },
-    canGenerate() {
-      if ( ! this.form.name) {
-        return false;
-      }
-
-      if ( this.form.name.split(' ').filter((part) => part).length <= 1 ) {
-        return false;
-      }
-
-      if ( ! this.form.jobTitle.trim() ) {
-        return false;
-      }
-
-      return true;
+    title() {
+      return this.canGenerate ? '' : 'Please fill up the form first.'
     }
   }
 }
