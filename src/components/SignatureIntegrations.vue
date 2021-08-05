5e5efa07c329ac7db71553bd282e05d586de0712
shazzad <sajib1223@gmail.com>
2021-08-05 20:18:35 +0600

    Added periods.

--- src/components/SignatureIntegrations.vue
+++ src/components/SignatureIntegrations.vue
@@ -7,21 +7,21 @@
       <li>Select Signature>Manage Signatures.</li>
       <li>Select the + sign, paste your signature and give it a name in Signature Name field.</li>
       <li>Click save.</li>
+      <li>Choose your new default signature for New messages and Replies/forwards.</li>
     </ol>
-
     <h2>Add signature in Gmail</h2>
     <ol>
       <li>Click on Copy Signature and go to your work Gmail account.</li>
       <li>Select the Settings gear in your Gmail toolbar.</li>
-      <li>Select See All Settings > General>Signature</li>
-      <li>Select + Create New button</li>
-      <li>Give this signature a name and select Create</li>
+      <li>Select See All Settings > General>Signature.</li>
+      <li>Select + Create New button.</li>
+      <li>Give this signature a name and select Create.</li>
       <li>Paste the signature in the box on the right and set siganture defaults.</li>
+      <li>Select your new default signature under Signature defaults.</li>
       <li>Scroll to bottom and select Save Changes.</li>
     </ol>
   </div>
 </template>
-
 
 <script>
 export default {
